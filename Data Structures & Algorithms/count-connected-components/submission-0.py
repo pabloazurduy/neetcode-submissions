89086bc0351527abcf01f6bbb313b4e6f1aa33cf
Pablo Azurduy <pablo.azurduy@ug.uchile.cl>
2026-08-14 22:41:37 +0200

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        
+
+        visited:List[int] = []
+
+        def dfs(i:int) -> None:
+            nonlocal visited
+            visited.append(i)
+            
+            neighboors = [sum(e)-i for e in edges if i in e and sum(e)-i not in visited]
+
+            if len(neighboors) == 0:
+                return 
+
+            for j in neighboors:
+                dfs(j)
+
+
+        nodes = list(range(n))
+        components = 0
+        while len(visited) < len(nodes):
+            for i in nodes:
+                if i not in visited:
+                    dfs(i)
+                    components+=1
+
+        return components
